2f97c2c53e83d01d69a54bcfe3b81847c1c13f47
IlyushinDM <ilyushin_dm@outlook.com>
2026-01-29 15:14:14 +0300

    feat: initial project setup and configuration

---
+++ tests/test_calculator.py
@@ -0,0 +1,9 @@
+from src.project_to_modify.calculator import add, subtract
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+def test_subtract():
+    assert subtract(10, 5) == 5
+    assert subtract(2, 3) == -1
